b918559a14be2ac274ae3ea65783103676fe0fbc
tjay1505 <119523945+tjay1505@users.noreply.github.com>
2023-04-15 08:44:20 +0530

    trignal frist commit

---
+++ src/Tirheader.js
@@ -0,0 +1,17 @@
+import './sass/tir.css'
+
+function Tirheader(){
+    return(
+        <div className="Tir_header" >
+            <div className='header_logo' >
+                TIRGNAL
+            </div>
+
+            <div className='header_dno' >
+
+            </div>
+        </div>
+    );
+}
+
+export default Tirheader ;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tirheader from './Tirheader';
+function Asset(){
+  return <p>hi welcome</p>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Tirheader />
+    <Asset />
   </React.StrictMode>
 );
 
